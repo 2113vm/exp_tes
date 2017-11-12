10dd658228a3b630fb76067eaecdfb912ae7a0f3
Vitaly Markov <21138532110q@gmail.com>
2017-11-12 17:05:12 +0500

    Add progressBar

--- pipeline.py
+++ pipeline.py
@@ -3,12 +3,13 @@ import sys
 
 import cv2
 import pandas as pd
+from progressbar import ProgressBar
 
 from src.preprocessing import preprocessing
 from src.pytess import recognize_text
 from src.metrics import text_recognition_score
 
-OBJECT_NAME = 'pricetag'
+OBJECT_NAME = 'stella'
 IMAGES_DIR = 'data/crop_images_' + OBJECT_NAME + '/'
 METADATA_DIR = 'data/csv/'
 METADATA = 'df_crop_' + OBJECT_NAME
@@ -19,17 +20,22 @@ image_names = df_metadata['filename'].tolist()
 
 result = []
 
-for image_name in image_names:
+with ProgressBar(max_value=len(image_names)) as bar:
+    bar.update(0)
 
-    image = cv2.imread(path.join(sys.path[1], IMAGES_DIR, image_name))
-    image = preprocessing(image=image)
+    for i, image_name in enumerate(image_names):
 
-    predicted_text = recognize_text(image=image)
-    target_text = df_metadata[df_metadata['filename'] == image_name]['attribute'].tolist()[0]
+        image = cv2.imread(path.join(sys.path[1], IMAGES_DIR, image_name))
+        image = preprocessing(image=image)
 
-    score = text_recognition_score(target_text=target_text, predicted_text=predicted_text)
+        predicted_text = recognize_text(image=image)
+        target_text = df_metadata[df_metadata['filename'] == image_name]['attribute'].tolist()[0]
 
-    result.append([image_name, target_text, predicted_text, score[0], score[1]])
+        score = text_recognition_score(target_text=target_text, predicted_text=predicted_text)
+
+        result.append([image_name, target_text, predicted_text, score[0], score[1]])
+
+        bar.update(i)
 
 df_result = pd.DataFrame(data=result, columns=['filename',
                                                'target_text',
